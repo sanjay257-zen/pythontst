659ba8291cab902b147235cc9ff23b5f3ed5d543
sanjay257-zen <senthilkumarsanjay257@gmail.com>
2026-02-16 17:05:41 +0800

    modifide file area

--- pythontest.py
+++ pythontest.py
@@ -1,6 +1,13 @@
 # string
-name = "bro code"
-age = 18 
-quantity = 2
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+gpa = float(input("Enter your GPA: "))
 
-print(f"you are {name} and you are {age} years old and you have {quantity} items")
+print(f"hello{name}")
+print(f"you are {age} years old")
+print(f"your GPA is {gpa}")
+
+lenght = float(input("Enter the length of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
+area = lenght * width
+print(f"The area of the rectangle is: {area}")
